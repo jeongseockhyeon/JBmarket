63210b7b6f013b3f6a2001224b1d1fb973a05e4b
jeongseockhyeon <jeong9260@naver.com>
2022-12-12 21:02:46 +0900

    commit

--- components/Talkarea.tsx
+++ components/Talkarea.tsx
@@ -47,14 +47,14 @@ export default function Talkarea() {
             id="messages"
             className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
           >
-            {messages.map((msg, i) => {
+            {messages.map((msg) => {
               return (
                 <div className="flex items-end justify-end">
                   <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
                     <div>
                       <span
                         className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white "
-                        key={i}
+                        key={msg.toString()}
                       >
                         {msg.message}
                       </span>
