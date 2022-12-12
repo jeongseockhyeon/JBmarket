0244cbdff7dd8ba12891164586bb4d12ef38058b
jeongseockhyeon <jeong9260@naver.com>
2022-12-12 19:53:20 +0900

    commit

--- pages/api/chat/socketio.ts
+++ pages/api/chat/socketio.ts
@@ -1,22 +1,22 @@
 import { Server } from 'socket.io'
-// import messageHandler from '../../../utils/messageHandler'
+import messageHandler from '../../../utils/messageHandler'
 
-// export default function SocketHandler(req, res) {
-//   if (res.socket.server.io) {
-//     console.log('Already set up')
-//     res.end()
-//     return
-//   }
+export default function SocketHandler(req, res) {
+  if (res.socket.server.io) {
+    console.log('Already set up')
+    res.end()
+    return
+  }
 
-//   const io = new Server(res.socket.server)
-//   res.socket.server.io = io
+  const io = new Server(res.socket.server)
+  res.socket.server.io = io
 
-//   const onConnection = (socket) => {
-//     messageHandler(io, socket)
-//   }
+  const onConnection = (socket) => {
+    messageHandler(io, socket)
+  }
 
-//   io.on('conection', onConnection)
+  io.on('conection', onConnection)
 
-//   console.log('Setting up socket')
-//   res.end()
-// }
+  console.log('Setting up socket')
+  res.end()
+}
